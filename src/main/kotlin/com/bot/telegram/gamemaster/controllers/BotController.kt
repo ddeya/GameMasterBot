e505db53f22605c5bff439b1cd3ef57166ffb540
Razor <rzrdeyafgh7@gmail.com>
2020-02-18 01:32:24 -0300

    Code refactoring.

--- src/main/kotlin/com/bot/telegram/gamemaster/controllers/BotController.kt
+++ src/main/kotlin/com/bot/telegram/gamemaster/controllers/BotController.kt
@@ -1,38 +1,38 @@
-package com.bot.telegram.gamemaster.controller
+package com.bot.telegram.gamemaster.controllers
 
 import com.bot.telegram.gamemaster.messages.BotMessage
 import com.bot.telegram.gamemaster.messages.Update
+import com.bot.telegram.gamemaster.services.TelegramAPI
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.postForObject
 import java.util.logging.Level
 import java.util.logging.Logger
 
-private const val API_ENDPOINT = "https://api.telegram.org/bot"
+const val API_ENDPOINT = "https://api.telegram.org/bot"
+const val TOKEN = "1084250149:AAFcGrGrmM5R3VZQkctvJ7fN4omriJP8YTw"
+
 private const val ECHO_COMMAND = "/echo"
-private const val TOKEN = "1084250149:AAFcGrGrmM5R3VZQkctvJ7fN4omriJP8YTw"
 
 @RestController
-class BotController {
+class BotController(val telegramAPI: TelegramAPI) {
 
-    val logger: Logger = Logger.getLogger("[GameMaster]")
+    private val logger: Logger = Logger.getLogger("[GameMaster]")
 
     @PostMapping("/$TOKEN")
     fun onUpdate(@RequestBody update: Update) {
         logger.log(Level.INFO, "Got update: $update")
         if (update.message != null) {
-            val chatID = update.message.chat?.id
+            val chatID = update.message.chat.id
             val text = update.message.text
             when {
-                text?.startsWith(ECHO_COMMAND) == true -> onEcho(chatID!!, text)
+                text?.startsWith(ECHO_COMMAND) == true -> onEcho(chatID, text)
             }
         }
     }
 
     private fun onEcho(chatID: Long, text: String) {
         val textToSend = text.removePrefix(ECHO_COMMAND).trimStart()
-        RestTemplate().postForObject<Any>("$API_ENDPOINT$TOKEN/sendMessage", BotMessage(chatID, textToSend))
+        telegramAPI.sendMessage(BotMessage(chatID, textToSend))
     }
 }
---
+++ src/main/kotlin/com/bot/telegram/gamemaster/services/TelegramAPI.kt
@@ -0,0 +1,27 @@
+package com.bot.telegram.gamemaster.services
+
+import com.bot.telegram.gamemaster.controllers.API_ENDPOINT
+import com.bot.telegram.gamemaster.controllers.TOKEN
+import com.bot.telegram.gamemaster.messages.BotMessage
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.postForObject
+import java.util.logging.Level
+import java.util.logging.Logger
+
+@Service
+class TelegramAPI(val apiUrl: String = API_ENDPOINT, val authToken: String = TOKEN) {
+    private val httpClient: RestTemplate = RestTemplate()
+    val logger: Logger = Logger.getLogger("[TelegramAPI]")
+
+    fun sendMessage(message: BotMessage): Any = logRequest(message) { data ->
+        httpClient.postForObject("$apiUrl$authToken/sendMessage", data)
+    }
+}
+
+private fun TelegramAPI.logRequest(data: Any? = null, request: (Any?) -> Any): Any {
+    logger.log(Level.INFO, "Sending Request: ${data ?: ""}")
+    val response = request(data)
+    logger.log(Level.INFO, "Request Response: $response")
+    return response
+}
