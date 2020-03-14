433dcea498ddd1c01e93aa675c8ae9c87b15a340
Razor <rzrdeyafgh7@gmail.com>
2020-03-14 14:13:33 -0300

    Added reply_to_message property Fixed Message ID not being parsed

--- src/main/kotlin/com/bot/telegram/gamemaster/messages/Message.kt
+++ src/main/kotlin/com/bot/telegram/gamemaster/messages/Message.kt
@@ -1,5 +1,15 @@
 package com.bot.telegram.gamemaster.messages
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class Message(val messageId: Int, val from: User?, val chat: Chat, val date: Date?, val text: String?)
+data class Message(
+    @JsonProperty("message_id")
+    val messageId: Int,
+    val from: User?,
+    val chat: Chat,
+    val date: Date?,
+    val text: String?,
+    @JsonProperty("reply_to_message")
+    val replyToMessage: Message?
+)
